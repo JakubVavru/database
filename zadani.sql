1d428697e41f332d8f2635bed54527124d845c8f
JakubVavru <60426585+JakubVavru@users.noreply.github.com>
2020-11-04 10:14:16 +0100

    Update zadani.sql

--- zadani.sql
+++ zadani.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE carshop;
-
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 CREATE TABLE cars (
  id_car int NOT NULL AUTO_INCREMENT,
  manifacturer varchar(255) ,
@@ -7,9 +7,21 @@ CREATE TABLE cars (
  PRIMARY KEY (id_car)
 );
 
-INSERT INTO garages (manifacturer, model)
+INSERT INTO cars (manifacturer, model)
 VALUES ('Lada','Niva');
 
+INSERT INTO cars (manifacturer, model)
+VALUES ('Ford','Mustang');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Audi','R8');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('BMV','M8');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Škona','Yeti');
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 CREATE TABLE employees (
   id_emloyee int NOT NULL AUTO_INCREMENT,
   jmeno varchar(255),
@@ -22,6 +34,18 @@ CREATE TABLE employees (
 INSERT INTO employees (jmeno, příjmení, věk, email)
 VALUES ('Jakub','Vávrů',16,'jakub.vavru@student.ossp.cz');
 
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Jan','Šlechta',16,'jan.slechta@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Štěpán','Kolarovsky',16,'stepan.kolarovsky@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Michal','Plaček',16,'michal.placek@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Martin','Kokeš',16,'martin.kokes@ossp.cz');
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 CREATE TABLE garages (
  id_garage int NOT NULL AUTO_INCREMENT, /*AUTO_INCREMENT sám vytcáří čísla postupně*/
  kapacity int,
@@ -35,8 +59,28 @@ CREATE TABLE garages (
 
 INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
 VALUES (2,'Tonda',776542345,'jakub.vavru@student.ossp.cz','malá, zelená','Pučery 46');
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+UPDATE employees
+SET id_car = 1
+WHERE id_emloyee = 1;
 
-DESCRIBE garages;
+UPDATE employees
+SET id_car = 2
+WHERE id_emloyee = 3;
+
+UPDATE employees
+SET id_car = 3
+WHERE id_emloyee = 2;
+
+UPDATE employees
+SET id_car = 4
+WHERE id_emloyee = 5;
+
+UPDATE employees
+SET id_car = 5
+WHERE id_emloyee = 4;
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+DESCRIBE employees;
 
 SELECT *
-FROM garages ;
+FROM employees ;
