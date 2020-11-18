4a088ebb8eeb6195947d0cc6342825190688b75b
JakubVavru <60426585+JakubVavru@users.noreply.github.com>
2020-11-18 16:48:53 +0100

    Update zadani.sql

--- zadani.sql
+++ zadani.sql
@@ -20,6 +20,36 @@ INSERT INTO cars (manifacturer, model)
 VALUES ('BMV','M8');
 
 INSERT INTO cars (manifacturer, model)
+VALUES ('Škoda','Yeti');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Nimbus','3000');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Ford','F150');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Mazda','RX7');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Škoda','Favorit');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Škoda','Roomster');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Infiniti','Q50');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Marvel','Spiderman suit');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Lamborghini','Urus');
+
+INSERT INTO cars (manifacturer, model)
+VALUES ('Ferrari','488');
+
+INSERT INTO cars (manifacturer, model)
 VALUES ('Škona','Yeti');
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 CREATE TABLE employees (
@@ -44,7 +74,52 @@ INSERT INTO employees (jmeno, příjmení, věk, email)
 VALUES ('Michal','Plaček',16,'michal.placek@student.ossp.cz');
 
 INSERT INTO employees (jmeno, příjmení, věk, email)
-VALUES ('Martin','Kokeš',16,'martin.kokes@ossp.cz');
+VALUES ('Martin','Kokeš',21,'martin.kokes@ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Ivan','Křídlo',18,'ivan.kridlo@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Jakub','Hlen',21,'jakub.hlen@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Luděk','Evan',88,'ludek.evan@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Igor','Hnízdo',30,'igor.hnizdo@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Emil','Nemil',53,'emil.nemil@ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Ferda','Novák',19,'ferda.novak@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Bert','Luba',23,'bert.luba@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Martin','Spadni',69,'martin.spadni@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Bob','Ross',80,'++.placek@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Jan','Šlechtopád',45,'jan.slechtopad@ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Michal','Kok',55,'michal.kok@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Gordon','Ramsey',54,'gordon.ramsey@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Gordon','Freeman',16,'gordon.freeman@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Barney','Stinson',45,'michal.placek@student.ossp.cz');
+
+INSERT INTO employees (jmeno, příjmení, věk, email)
+VALUES ('Garry','Potter',78,'garry.potter@ossp.cz');
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 CREATE TABLE garages (
  id_garage int NOT NULL AUTO_INCREMENT, /*AUTO_INCREMENT sám vytcáří čísla postupně*/
@@ -59,6 +134,33 @@ CREATE TABLE garages (
 
 INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
 VALUES (2,'Tonda',776542345,'jakub.vavru@student.ossp.cz','malá, zelená','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Karel',776542345,'jakub.vavru@student.ossp.cz','malá, červená','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Ema',776542345,'jakub.vavru@student.ossp.cz','malá, modrá','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Lada',776542345,'jakub.vavru@student.ossp.cz','malá, žlutá','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Poko',776542345,'jakub.vavru@student.ossp.cz','malá, zelená','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Kuba',776542345,'jakub.vavru@student.ossp.cz','malá, zelená','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Emil',776542345,'jakub.vavru@student.ossp.cz','malá, oranžová','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Jirka',776542345,'jakub.vavru@student.ossp.cz','malá, žlutá','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Jiřina',776542345,'jakub.vavru@student.ossp.cz','malá, modrá','Pučery 46');
+
+INSERT INTO garages (kapacity, garage_name, telephone, email, garage_description, adress)
+VALUES (2,'Tary',776542345,'jakub.vavru@student.ossp.cz','malá, červená','Pučery 46');
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 UPDATE employees
 SET id_car = 1
